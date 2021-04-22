a9c885082b087f34f3122e931e4314f201454801
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-04-22 15:28:40 -0400

    Paradice pools

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Button } from '../ButtonElements';
+
+import Palm from '../../images/palm.jpeg';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements';
+
+function HeroSection() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <img src={Palm} width="100%" height="100%" alt="Palm" />;
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Paradise Pools</HeroH1>
+        <HeroP>
+          Reliable Service For 100% Pool Enjoyment
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default HeroSection;
